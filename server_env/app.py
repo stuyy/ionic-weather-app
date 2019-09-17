995bbfc765f5dbac6f373efa536f79bdd1a2f656
Anson Foong <afoong@albany.edu>
2019-09-17 04:23:16 -0400

    Use the nomatin api from openstreetmap to get the state name

--- server_env/app.py
+++ server_env/app.py
@@ -2,7 +2,7 @@ from sanic import Sanic
 import sanic.response as response
 import requests_async as requests
 import json
-
+from Weather import Weather, OpenStreetMap
 app = Sanic()
 
 def load_api_key():
@@ -11,12 +11,23 @@ def load_api_key():
 
 API_KEY = load_api_key()
 
-@app.route("/")
-async def home(request):
-    weather = await requests.get("https://api.openweathermap.org/data/2.5/weather?units=imperial&q=new%20york&appid={}".format(API_KEY))
-    print(weather.json())
-    return response.json(weather.json())
+weather = Weather(API_KEY)
+osm = OpenStreetMap()
 
+@app.route("/api/weather/<city>")
+async def home(request, city):
+    data = await weather.get_weather(city)
+    return response.json(data.json())
+
+@app.route("/api/forecast/<city>")
+async def forecast_route(request, city):
+    data = await weather.get_forecast(city)
+    coords = {
+        "lat" : data['city']['coord']['lat'],
+        "lon" : data['city']['coord']['lon']
+    }
+    data['city_data'] = await osm.check_location(coords['lat'], coords['lon'])
+    return response.json(data)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=3000, debug=True)
