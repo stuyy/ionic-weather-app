da5c3bf1b5985cdccbf63fd685637232b7d56d0f
Anson Foong <afoong@albany.edu>
2019-09-17 04:34:19 -0400

    Added OpenStreetMap class.

--- server_env/Weather.py
+++ server_env/Weather.py
@@ -14,3 +14,20 @@ class Weather:
         url = "{}{}{}{}{}{}".format(self.forecast_base_url, city_name, "&appid=", self.token, "&units=", self.units)
         print(url)
         return (await requests.get(url)).json()
+
+class OpenStreetMap:
+    def __init__(self):
+        self.url = 'https://nominatim.openstreetmap.org/reverse?format=json&'
+
+    async def get_location(self, lat, lon):
+        url = "{}{}{}{}{}".format(self.url, "lat=", lat, "&lon=", lon)
+        return (await requests.get(url)).json()['address']
+
+    async def get_state(self, lat, lon):
+        data = await self.get_location(lat, lon)
+        return data['address']['state']
+
+    async def get_city(self, lat, lon):
+        data = await self.get_location(lat, lon)
+        return data['address']['city']
+
