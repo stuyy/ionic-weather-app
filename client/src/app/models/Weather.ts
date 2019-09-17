1d4fe6e5fe90bbfbfc0955bac4d99a7b7e32022f
Anson Foong <afoong@albany.edu>
2019-09-17 05:17:29 -0400

    Added a few extra fields to Weather model.

--- client/src/app/models/Weather.ts
+++ client/src/app/models/Weather.ts
@@ -1,6 +1,10 @@
 export class Weather {
     public city: string;
+    public state: string;
     public country: string;
     public temperature: Number;
     public icon: string;
+    public conditions: Array<any>;
+    public maxTemp: Number;
+    public minTemp: Number;
 }
