749728049896394784d9be9b8c0aaa64fbf61546
Anson Foong <afoong@albany.edu>
2019-09-21 02:52:48 -0400

    Using local storage/websql to store all of the cities the user will want to get weather data from

--- client/src/app/pages/settings/settings.page.ts
+++ client/src/app/pages/settings/settings.page.ts
@@ -15,21 +15,30 @@ export class SettingsPage implements OnInit {
   }
 
   ngOnInit() {
-    this.currentLocations = [
-      {
-        name: 'New York'
-      },
-      {
-        name: 'Toronto'
-      }
-    ]
+    this.storage.get('settings')
+    .then(res => this.currentLocations = res)
+    .catch(err => console.log(err));
   }
 
-  addNewLocation(event) {
-    console.log(this.cityName);
-    this.currentLocations.push({
-      name: this.cityName
-    })
+  async addNewLocation(event) {
+    try {
+      const settings = await this.storage.get('settings');
+      if(settings !== null) {
+        console.log(settings);
+        if(settings.some(el => el.toLowerCase() === this.cityName.toLowerCase())) {
+          console.log('Duplicate');
+        } else {
+          settings.push(this.cityName); // Push the new city to the array.
+          this.storage.set('settings', settings);
+          this.currentLocations = settings;
+        }
+      } else {
+        this.storage.set('settings', []);
+      }
+    }
+    catch(err) {
+
+    }
   }
   toggleDialog(event) {
     this.displayDialog = !this.displayDialog;
